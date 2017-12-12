c39ad323fa9f52840fb6c865c6cf318e00ef1f57
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-12-12 14:15:20 +0900

    動くけど・・・

--- token.c
+++ token.c
@@ -1,73 +1,92 @@
 #include<stdio.h>
+#include <ctype.h>
 
-char outtoken[200][50]; //token格納用配列
+char tokens[200][50];
 FILE *fi;
-int check_word(char);
 
-int main()
-{
-	int i,j,token_num;
-	char c;
-	fi=fopen("./pro1.p","r");
-	token_num=0;  //トークンの数の初期化
-	j=0;
-	while(1){
-		c=fgetc(fi);  //1文字ずつ入力
-		printf("%c\n",c);
-		if(c==EOF)break;
-
-		if(check_word(c)==0){//数字なら 
-			:
-			:
-		}
-		else if(check_word(c)==1){//文字なら 
-			:
-			:
-		}
-		else if(check_word(c)==2){  //改行，スペース，タブなら
-			if(j!=0){
-				outtoken[token_num][j]='\0';
-				j=0;
-				token_num++;
-			}
-			else{
-				//do nothing!!
-			}
-		}else if(check_word(c)==3){  //記号なら(1文字)
-			:
-			:
-		} else if(check_word(c)==4){  //記号なら(2文字)
-			:
-			:
-		}
-	}
-	
-	for(i=0;i<token_num;i++)
-		printf("%3d  %s\n",i,outtoken[i]);
-	return 0;
-}
-
-int check_word(char c)
-{
-	if(c>='0' && c<='9') // 数字なら 
+int check_word(char c){
+	if(isdigit(c))
 		return 0;
-	else if(....)        //アルファベット
+	else if(isalpha(c))
 		return 1;
-	else if(....)        //改行，スペース，タブ
+	else if(c=='\n' || c==' ' || c=='\t')
 		return 2;
-	else if(....)        //1文字のみの記号
+	else if((c>='(' && c<='/')||c==';'||c=='=')
 		return 3;
-	else if(c==':')      //コロンは'='と必ず一緒になる
+	else if(c==':')      //コロンは'='と必ず一緒になるので
 		return 4;
 	else if(c=='<' || c=='>'){  //この場合は次の文字を見る必要がある
 		c=fgetc(fi);
-		fseek(fi,-1,1);   //ファイルポインタを1つ戻す 
+		fseek(fi,-1,1);
 		if(c=='=' || c=='>'){
 			return 4;
 		}else{
 			return 3;
 		}
 	}
+        return -1;
 }
 
+int main(){
+	fi=fopen("p1","r");
+	int token_num=0;
+	while(1){
+		char c=fgetc(fi);
+		if(c==EOF){
+                        break;
+                }
+		printf("%c\n",c);
+		if(check_word(c)==0){//数字なら 
+                        int char_p=0;
+                        int string_length=0;
+                        do{
+                                string_length++;
+                                tokens[token_num][char_p]=c;
+                                char_p++;
+                                c=fgetc(fi);
+                        }while(check_word(c)==0);
+                        tokens[token_num][char_p]='\0';
+                        fseek(fi,-1,SEEK_CUR);
+                        token_num++;
+		}
+		else if(check_word(c)==1){//文字なら 
+                        int char_p=0;
+                        int string_length=0;
+                        do{
+                                string_length++;
+                                tokens[token_num][char_p]=c;
+                                char_p++;
+                                c=fgetc(fi);
+                        }while(check_word(c)==1);
+                        tokens[token_num][char_p]='\0';
+                        fseek(fi,-1,SEEK_CUR);
+                        token_num++;
+		}
+		//else if(check_word(c)==2){  //改行，スペース，タブなら
+                //        int char_p=0;
+		//	if(char_p!=0){
+		//		tokens[token_num][char_p]='\0';
+		//		char_p=0;
+		//		token_num++;
+		//	}
+		//	else{
+		//		//do nothing!!
+		//	}
+		//}
+                else if(check_word(c)==3){  //記号なら(1文字)
+                        tokens[token_num][0]=c;
+                        tokens[token_num][1]='\0';
+                        token_num++;
+		} else if(check_word(c)==4){  //記号なら(2文字)
+                        tokens[token_num][0]=c;
+                        tokens[token_num][1]=fgetc(fi);
+                        tokens[token_num][2]='\0';
+                        token_num++;
+		}
+	}
+	
+	for(int i=0;i<token_num;i++){
+		printf("%3d  %s\n",i,tokens[i]);
+        }
+}
 
