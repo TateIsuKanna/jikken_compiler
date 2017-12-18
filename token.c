62012776d588e463aa434f619c938bd5d49852cf
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-12-19 00:26:13 +0900

    動く

--- token.c
+++ token.c
@@ -15,17 +15,15 @@ enum word_type{
 	word_type_2char,
 };
 
-int check_word(char c){
-	if(c=='\n' || c==' ' || c=='\t'){
+int check_word(char* c){
+	if(*c=='\n' || *c==' ' || *c=='\t'){
 		return word_type_separator;
-	}else if((c>='(' && c<='/')||c==';'||c=='='){
+	}else if((*c>='(' && *c<='/')||*c==';'||*c=='='){
 		return word_type_1char;
-	}else if(c==':'){
+	}else if(*c==':'){
 		return word_type_2char;
-	}else if(c=='<' || c=='>'){
-		c=fgetc(fi);
-		fseek(fi,-1,1);
-		if(c=='=' || c=='>'){
+	}else if(*c=='<' || *c=='>'){
+		if(c[1]=='=' || c[1]=='>'){
 			return word_type_2char;
 		}else{
 			return word_type_1char;
@@ -46,55 +44,56 @@ int main(int argc,char *argv[]){
 	if(!fi){
 		return -1;
 	}
+	char* line_str=NULL;
+	size_t line_str_size=0;
 	while(1){
-		char* line_str=NULL;
-		size_t line_str_size=0;
 		if(getline(&line_str,&line_str_size,fi)==EOF){
 			break;
 		}
-		for(int line_p=0;line_p<strlen(line_str);){
-			char c=line_str[line_p++];
-			if(isdigit(c)){
-				int char_p=0;
+		int line_p=0;
+		while(1){
+			if(isdigit(line_str[line_p])){
 				vector_push(&tokens);
-				do{
-					tokens.data[tokens.size-1][char_p++]=c;
-					c=line_str[line_p++];
-				}while(isdigit(c));
-				tokens.data[tokens.size-1][char_p]='\0';
-				fseek(fi,-1,SEEK_CUR);
+				int word_end_g=line_p;
+				while(isdigit(line_str[++word_end_g]));
+				strncpy(tokens.data[tokens.size-1],line_str+line_p,word_end_g-line_p);
+				tokens.data[tokens.size-1][word_end_g-line_p]='\0';
+				line_p+=word_end_g-line_p;
 			}
-			else if(isalpha(c)){
-				int char_p=0;
+			else if(isalpha(line_str[line_p])){
 				vector_push(&tokens);
-				do{
-					tokens.data[tokens.size-1][char_p++]=c;
-					c=line_str[line_p++];
-				}while(isdigit(c)||isalpha(c));
-				tokens.data[tokens.size-1][char_p]='\0';
-				fseek(fi,-1,SEEK_CUR);
-			}else if(check_word(c)==word_type_1char){
+				int word_end_g=line_p;
+				//数字か文字か両方確認してから++
+				while(isdigit(line_str[word_end_g])||isalpha(line_str[++word_end_g]));
+				strncpy(tokens.data[tokens.size-1],line_str+line_p,word_end_g-line_p);
+				tokens.data[tokens.size-1][word_end_g-line_p]='\0';
+				line_p+=word_end_g-line_p;
+			}else if(check_word(line_str+line_p)==word_type_1char){
 				vector_push(&tokens);
-				tokens.data[tokens.size-1][0]=c;
+				tokens.data[tokens.size-1][0]=line_str[line_p++];
 				tokens.data[tokens.size-1][1]='\0';
-			}else if(check_word(c)==word_type_2char){
+			}else if(check_word(line_str+line_p)==word_type_2char){
 				vector_push(&tokens);
-				tokens.data[tokens.size-1][0]=c;
+				tokens.data[tokens.size-1][0]=line_str[line_p++];
 				tokens.data[tokens.size-1][1]=line_str[line_p++];
 				tokens.data[tokens.size-1][2]='\0';
+			}else if(line_str[line_p]=='\n'||line_str[line_p]=='\r'){
+				break;
+			}else{
+				line_p++;
 			}
 		}
-		free(line_str);
 	}
+	free(line_str);
 
 	//TODO:この辺関数で切り分け，あと全体的にHACK
 	for(int i=0;i<tokens.size;i++){
 		printf("%3d  %s\t",i,tokens.data[i]);
-		int is_reserved_word=0;
+		bool is_reserved_word=false;
 		for(int word_search_i=0;word_search_i<sizeof(reserved_words)/sizeof(char*);++word_search_i){
 			if(strcmp(reserved_words[word_search_i],tokens.data[i])==0){
 				printf("予約語\n");
-				is_reserved_word=1;
+				is_reserved_word=true;
 				break;
 			}
 		}
@@ -103,7 +102,7 @@ int main(int argc,char *argv[]){
 		}
 		if(isdigit(tokens.data[i][0])){
 			printf("整数");
-		}else if(check_word(tokens.data[i][0])>=word_type_1char){//HACK:特に!
+		}else if(check_word(tokens.data[i])>=word_type_1char){//HACK:特に!
 			printf("記号");
 		}else{
 			printf("名前");
