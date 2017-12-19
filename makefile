cf7cccb7cea8e44e55c0e25da1f0d8b65e6ca8fb
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-12-19 16:47:03 +0900

    まだ不安定

--- makefile
+++ makefile
@@ -1,11 +1,14 @@
 ARG=
-CFLAGS=-Wall --std=c11
+CFLAGS=-Wall -g --std=c11
 
 pcp:token.c vector
 	gcc $(CFLAGS) -o pcp vector token.c
 
 vector:vector.*
 	gcc $(CFLAGS) -c -o vector vector.c
+
+clean:
+	rm vector pcp
 
 run:pcp
 	./pcp $(ARG)
--- token.c
+++ token.c
@@ -125,13 +125,11 @@ enum token_type get_token_type(char* token){
         }
         for(int word_search_i=0;word_search_i<sizeof(reserved_words)/sizeof(char*);++word_search_i){
                 if(strcmp(reserved_words[word_search_i],token)==0){
-                        printf("予約語\n");
                         return token_type_reserved;
                 }
         }
         for(int word_search_i=0;word_search_i<sizeof(sign_words)/sizeof(char*);++word_search_i){
                 if(strcmp(sign_words[word_search_i],token)==0){
-                        printf("記号\n");
                         return token_type_mark;
                 }
         }
@@ -148,6 +146,7 @@ void exit_by_error(char* str){
         printf("error %s",str);
         exit(-1);
 }
+
 
 void const_decl(){
         printf("begin const_decl\n");
@@ -174,25 +173,223 @@ void const_decl(){
         }
 }
 void var_decl(){
+        printf("begin var_decl");
+        do{
+                current_token_i++;
+                if(get_token_type(tokens.data[current_token_i])!=token_type_ident){
+                        exit_by_error("var_decl token_type_ident");
+                }
+                current_token_i++;
+        }while(iseqstr(tokens.data[current_token_i],","));
+        if(tokens.data[current_token_i][0]==';'){
+                printf("end var_decl\n");
+                return;
+        }else{
+                exit_by_error("var_decl ;");
+        }
 }
 void func_decl(){
+        printf("begin func_decl\n");
+        current_token_i++;
+        if(get_token_type(tokens.data[current_token_i])!=token_type_ident){
+                exit_by_error("func_decl token_type_ident");
+        }
+        current_token_i++;
+        if(tokens.data[current_token_i][0]!='('){
+                exit_by_error("func_decl (");
+        }
+        current_token_i++;
+        //HACK:
+        if(get_token_type(tokens.data[current_token_i])==token_type_ident){
+                while(1){
+                        if(get_token_type(tokens.data[current_token_i])!=token_type_ident){
+                                exit_by_error("func_decl token_type_ident");
+                        }
+                        current_token_i++;
+                        if(!iseqstr(tokens.data[current_token_i],",")){
+                                break;
+                        }
+                        current_token_i++;
+                }
+        }else{
+                current_token_i--;//TODO:どういうこと?????
+        }
+        if(tokens.data[current_token_i][0]!=')'){
+                exit_by_error("func_decl )");
+        }
+        block();
+        current_token_i++;
+        if(tokens.data[current_token_i][0]==';'){
+                printf("end func_decl\n");
+                return;
+        }else{
+                exit_by_error("func_decl ;");
+        }
 }
-void statement(){
-}
-
 void block(){
+        printf("begin block\n");
+        current_token_i++;
         while(1){
                 if(iseqstr(tokens.data[current_token_i],"const")){
+                        printf("begin const_decl\n");
                         const_decl();
                 }else if(iseqstr(tokens.data[current_token_i],"var")){
+                        printf("begin var_decl\n");
                         var_decl();
                 }else if(iseqstr(tokens.data[current_token_i],"function")){
+                        printf("func_decl\n");
                         func_decl();
                 }else{
                         break;
                 }
         }
         statement();
+        printf("end block\n");
+}
+
+
+void factor(){
+        printf("begin factor\n");
+        current_token_i++;
+        if(get_token_type(tokens.data[current_token_i])!=token_type_ident){
+                current_token_i++;
+                if(iseqstr(tokens.data[current_token_i],"(")){
+                        //TODO:
+                }else{
+                        return;
+                }
+        }
+        if(get_token_type(tokens.data[current_token_i])!=token_type_integer){
+                return;
+        }
+        if(tokens.data[current_token_i][0]=='('){
+                expression();
+                if(tokens.data[current_token_i][0]==')'){
+                        return;
+                }else{
+                        exit_by_error("factor )");
+                }
+        }
+}
+
+void term(){
+        printf("begin term\n");
+        factor();
+        while(1){
+                current_token_i++;
+                if(tokens.data[current_token_i][0]=='*'||
+                tokens.data[current_token_i][0]=='/'){
+                }else{
+                        break;
+                }
+                factor();
+        }
+        printf("end term\n");
+}
+
+void expression(){
+        printf("begin expression\n");
+        current_token_i++;
+        if(tokens.data[current_token_i][0]=='+'||
+           tokens.data[current_token_i][0]=='-'){
+        }else{
+                current_token_i--;//TODO:どういうこと?????
+        }
+        term();
+        while(1){
+                current_token_i++;
+                if(tokens.data[current_token_i][0]=='+'||
+                tokens.data[current_token_i][0]=='-'){
+                }else{
+                        break;
+                }
+                term();
+        }
+        printf("end expression\n");
+}
+
+void condition(){
+        printf("begin condition\n");
+        current_token_i++;
+        if(iseqstr(tokens.data[current_token_i],"odd")){
+                expression();
+                return;
+        }
+        current_token_i--;//TODO:どういうこと?????
+        expression();
+        current_token_i++;
+        if(iseqstr(tokens.data[current_token_i],"=")||
+                iseqstr(tokens.data[current_token_i],"<>")||
+                iseqstr(tokens.data[current_token_i],"<")||
+                iseqstr(tokens.data[current_token_i],">")||
+                iseqstr(tokens.data[current_token_i],"<=")||
+                iseqstr(tokens.data[current_token_i],">=")){
+                expression();
+        }else{
+                exit_by_error("condition =<><><=>=");
+        }
+}
+
+void statement(){
+        printf("begin statement\n");
+        current_token_i++;
+        if(get_token_type(tokens.data[current_token_i])==token_type_ident){
+                printf("ident\n");
+                current_token_i++;
+                if(iseqstr(tokens.data[current_token_i],":=")){
+                        expression();
+                }else{
+                        exit_by_error("statement :=");
+                }
+                return;
+        }
+        if(iseqstr(tokens.data[current_token_i],"begin")){
+                printf("begin\n");
+                while(1){
+                        statement();
+                        current_token_i++;
+                        if(tokens.data[current_token_i][0]!=';'){
+                                break;
+                        }
+                }
+                current_token_i--;//TODO:どういうこと?????
+
+                if(!iseqstr(tokens.data[current_token_i],"end")){
+                        exit_by_error("statement end");
+                }
+                return;
+        }
+        if(iseqstr(tokens.data[current_token_i],"if")){
+                condition();
+                current_token_i++;
+                if(!iseqstr(tokens.data[current_token_i],"then")){
+                        exit_by_error("statement then");
+                }
+                statement();
+                return;
+        }
+        if(iseqstr(tokens.data[current_token_i],"while")){
+                condition();
+                current_token_i++;
+                if(!iseqstr(tokens.data[current_token_i],"do")){
+                        exit_by_error("statement do");
+                }
+                statement();
+                return;
+        }
+        if(iseqstr(tokens.data[current_token_i],"return")){
+                expression();
+                return;
+        }
+        if(iseqstr(tokens.data[current_token_i],"write")){
+                expression();
+                return;
+        }
+        if(iseqstr(tokens.data[current_token_i],"writeln")){
+                return;
+        }else{
+                current_token_i--;//TODO:どういうこと?????
+        }
 }
 
 int main(int argc,char *argv[]){
@@ -208,4 +405,11 @@ int main(int argc,char *argv[]){
         get_token();
 
         block();
+
+        current_token_i++;
+        if(tokens.data[current_token_i][0]=='.'){
+                printf("completed\n");
+        }else{
+                exit_by_error("last . error");
+        }
 }
