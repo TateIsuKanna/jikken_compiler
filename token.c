aed36769af49a759f2623ee4cfc4a691dd82f610
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-12-27 10:25:36 +0900

    No such file or directory

--- token.c
+++ token.c
@@ -455,6 +455,7 @@ int main(int argc,char *argv[]){
 
 	fi=fopen(argv[1],"r");
 	if(!fi){
+		fprintf(stderr,"error: %s: No such file or directory\n",argv[1]);
 		return -1;
 	}
         get_token();
